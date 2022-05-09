9f4ada493cce92467dbf7722f7f1f4ade50a1677
EvgeniyKurlyanchik <91161381+EvgeniyKurlyanchik@users.noreply.github.com>
2022-05-09 16:52:30 +0300

    Initial commit

--- server/src/main/java/module-info.java
+++ server/src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module ru.gb.server {
     requires org.slf4j;
     requires java.sql;
-    requires lombok;
+
     requires io.netty.transport;
     requires io.netty.codec;
     requires io.netty.common;
